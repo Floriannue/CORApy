67b04ff0afc348b4f8fcc6f308368b942ad6d54b
Tiange Yang <tgyang@sjtu.edu.cn>
2025-07-21 23:14:41 +0200

    corrected conZonotope

--- cora_python/contSet/zonotope/conZonotope.py
+++ cora_python/contSet/zonotope/conZonotope.py
@@ -1,5 +1,29 @@
 """
-conZonotope method for zonotope class
+conZonotope - convert a zonotope object into a conZonotope object
+
+Syntax:
+    cZ = conZonotope(Z)
+
+Inputs:
+    Z - zonotope object
+
+Outputs:
+    cZ - conZonotope object
+
+Example:
+    Z = zonotope([1;0],[1 0; 0 1])
+    cZ = conZonotope(Z)
+
+Other m-files required: none
+Subfunctions: none
+MAT-files required: none
+
+See also: none
+
+Authors:       Niklas Kochdumper
+Written:       23-May-2018
+Last update:   ---
+Last revision: ---
 """
 
 from .zonotope import Zonotope
